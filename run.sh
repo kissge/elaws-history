22f89637c53a982e1ef405148b8f4457682b964b
Yusuke Kido <mail@yusuk.eki.do>
2024-05-06 20:37:17 +0900

    User Agent文字列をソースコード中で管理

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ export ALL_XML_DIRECTORY_NAME=all_xml
 export NOW="$WORKDIR"/now
 export HEADERS="$WORKDIR"/headers
 export EH_VERSION=$(git describe)
+export USER_AGENT="Mozilla/5.0 (compatible; elaws-history/$EH_VERSION; +https://github.com/kissge/elaws-history)"
 
 bash src/cleanup.sh
 bash src/get-files.sh
