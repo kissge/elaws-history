a84c4b4a77ed83c634e215e637c24e494c637701
kissge <mail@yusuk.eki.do>
2024-07-30 14:23:19 +0900

    e-Gov法令検索のURL変更対応

--- src/get-files.sh
+++ src/get-files.sh
@@ -5,7 +5,7 @@ set -Eeuxo pipefail
 mkdir -p "$WORKDIR"
 TZ=Asia/Tokyo date +%Y/%m/%dT%H%M%S.%N%z >"$NOW"
 zip="$WORKDIR"/all_xml.zip
-curl -D "$HEADERS" -A "$USER_AGENT" -o "$zip" 'https://elaws.e-gov.go.jp/download?file_section=1&only_xml_flag=true'
+curl -D "$HEADERS" -A "$USER_AGENT" -o "$zip" 'https://laws.e-gov.go.jp/bulkdownload?file_section=1&only_xml_flag=true'
 
 mkdir -p "$GIT_ROOT" && cd $_
 mkdir -p "$ALL_XML_DIRECTORY_NAME" && cd $_
